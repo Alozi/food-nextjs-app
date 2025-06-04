4975bb5a0f166b85de98a7165d765a12efa66dfc
Alozi <alozimozi@gmail.com>
2025-06-04 18:27:34 +0300

    Add active class to the header

--- components/header/header.js
+++ components/header/header.js
@@ -1,11 +1,17 @@
+'use client';
+
 import Image from "next/image";
 import Link from "next/link";
 
 import HeaderBackground from "./header-background.js";
 import classes from './header.module.css';
 import logoImage from '@/assets/logo.png'
+import { usePathname } from "next/navigation.js";
 
 export default function Header() {
+    const path = usePathname();
+    console.log(path);
+
     return (
         <>
             <HeaderBackground />
@@ -16,10 +22,11 @@ export default function Header() {
                 </Link>
                 <nav className={classes.nav}>
                     <ul>
-                        <li><Link href="/meals">Browse Meals</Link></li>
-                        <li><Link href="/community">Foodies Community</Link></li>
+                        <li><Link href="/meals" className={path.startsWith('/meals') ? classes.active : undefined}>Browse Meals</Link></li>
+                        <li><Link href="/community" className={path === '/community' ? classes.active : undefined}>Foodies Community</Link></li>
                     </ul>
                 </nav>
             </header>
-        </>)
+        </>
+    );
 }
