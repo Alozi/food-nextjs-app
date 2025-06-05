55c2c8481c73cf239b58ba6d8371e95d8c4d272d
Alozi <alozimozi@gmail.com>
2025-06-05 13:40:00 +0300

    Use database on project and show items from db

--- components/meals/meals-grid.js
+++ components/meals/meals-grid.js
@@ -4,8 +4,6 @@ import classes from './meals-grid.module.css';
 export default function MealsGrid({ meals }) {
     return (<ul className={classes.meals}>
         {meals.map(item => {
-            console.log(item);
-
             return (
                 <li key={item.id}>
                     <MealItem {...item} />
